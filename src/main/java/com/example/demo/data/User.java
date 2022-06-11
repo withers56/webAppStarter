161fd36fbcf98e2e93e16d7e62f061989914ddf8
withers56 <william.k.withers@gmail.com>
2022-06-11 17:05:34 -0500

    first commit mes

---
+++ src/main/java/com/example/demo/data/User.java
@@ -0,0 +1,47 @@
+package com.example.demo.data;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Collection;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+
+    @Email
+    @NotEmpty
+    private String email;
+
+//    @Column(nullable = false)
+    @ToString.Exclude
+    private String password;
+
+    @Column
+    private LocalDate createdAt;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {USER, ADMIN}
+}
---
+++ src/main/java/com/example/demo/data/UsersRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+    User findByEmail(String email);
+
+}
